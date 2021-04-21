2ccbe2d33465406a62177bf8aa37c5b0bdea0e0d
Bruno <bruno.umbelino0@gmail.com>
2021-04-21 10:44:17 -0300

    day two

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express, { request, response } from "express";
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
-app.get('/', (request , response) =>{
-  return response.json({
-    message: "olá terráqueos"
-  })
-}) 
+app.use(express.json())
 
-app.post("/", (request, response)=>{
-  return response.json({message: "Usuário cadastrado com sucesso!"})
-})
+app.use(routes);
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "olá terráqueos",
+  });
+});
 
 app.listen(3333, () => console.log("server is running on port 3333"));
